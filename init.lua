138c08769dfe74806fb3d986bcbde47cf664a671
Ashish Dwivedi <dwivediashish.work@gmail.com>
2025-08-01 15:57:13 +0530

    Add blur background in nvim

--- init.lua
+++ init.lua
@@ -261,6 +261,8 @@ require('lazy').setup({
     },
   },
 
+  { 'typicode/bg.nvim', lazy = false },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
